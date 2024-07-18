39c083440ddbae9394eb7936ac681f27dc0d38e7
Aldo Salas <aldosalasrdz@gmail.com>
2024-07-17 18:49:38 -0600

    Revert health check path and change port

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
     end
   end
 
-  # Defines the root path route ("/")
-  root "main#index"
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "/" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "main#index"
 end
